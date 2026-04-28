e1961943422366f3ca8e939950018b1c63e055d4
Paul Gálvez <paul.galvez@usm.cl>
2026-04-28 10:19:08 -0400

    Configurar conexión PostgreSQL en database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./reservas.db"
+DATABASE_URL = "postgresql://database_reserva_db_user:MRO8wVhLSjZeo0vi2BrDEvR5iqjyZzDM@dpg-d7oamsdckfvc73fgjp50-a:5432/database_reserva_db"
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
