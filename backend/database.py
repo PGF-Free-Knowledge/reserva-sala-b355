a1366547deef9ae57fdd9d9e496499ef8954743b
Paul Gálvez <paul.galvez@usm.cl>
2026-04-28 11:23:55 -0400

    Fix: remove check_same_thread for PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "postgresql://database_reserva_db_user:MRO8wVhLSjZeo0vi2BrDEvR5iqjyZzDM@dpg-d7oamsdckfvc73fgjp50-a:5432/database_reserva_db"
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+Base = declarative_base
