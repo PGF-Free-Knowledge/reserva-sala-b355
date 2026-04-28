da5de37a74683964e661eea125c045079081172a
Paul Gálvez <paul.galvez@usm.cl>
2026-04-28 14:28:42 -0400

    Migración completa a Postgres, eliminado SQLite

--- backend/main.py
+++ backend/main.py
@@ -18,11 +18,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Configuración SQLite
-DATABASE_URL = "sqlite:///reservas.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Previo
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Configuración Postgres (Render)
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 # Modelo de reservas
 class Reserva(Base):
